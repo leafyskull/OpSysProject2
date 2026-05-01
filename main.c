40e1c413594f945880d16ca2555b3a7e4348244e
leafyskull <danielholt505@gmail.com>
2026-05-01 11:02:47 -0500

    Added thread_args struct for passing data to new thread

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 // File: main.c
 // Author: Danny Holt
 // This file contains the main function for the "Greasy cards" game.
-#define _POSIX_C_SOURCE 200809L
+
+#define _POSIX_C_SOURCE 200809L // Might remove
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
@@ -9,13 +10,40 @@
 #include <stdlib.h>
 
 
+// This struct will hold info to be passed to each player thread.
+struct thread_args {
+    int playerID;
+    int numRounds;
+};
+
+
 // playerThread: Logic for each player thread
 //
 // *arg: Pointer to the player's ID
-void *playerThread(void *arg) {
-    long playerID = *(int *)arg;
+void *playerThread(void *newThreadArgs) {
+
+    struct thread_args *newThreadData = (struct thread_args *)newThreadArgs;
+    int playerID = newThreadData->playerID;
+    int numRounds = newThreadData->numRounds;
+
     printf("Now in playerThread\n");
-    printf("Player %ld is starting their thread.\n", playerID);
+    printf("Player %d is starting their thread.\n", playerID);
+
+    // What does each player do?
+    // If they're the dealer:
+        // Shuffle cards
+        // Choose random card (Greasy card)
+        // Deal single card to each player
+        // Open first bag of potato chips
+        // Wait for round to end
+
+    // If they're not the dealer:
+        // Draw card from deack and compare to "greasy card"
+        // If no cards in hand are greasy card (match value), discard one card at random (place at end of deck).
+        // If there is a match, player shows hand, declares self as winner, and round ends.
+
+    // At the end of round "m", player "m" (dealer) will signal for next round to start.
+    // -> game ends when all [numPlayers] rounds are completed
 
     return NULL;
 }
@@ -36,43 +64,33 @@ int main() {
     // Number of rounds = number of players
     int numRounds = numPlayers;
 
-    // Creating a thread for each player
-    pthread_t threads[numPlayers];
-    int playerIDs[numPlayers];
+    pthread_t threads[numPlayers]; // Holds threads
+    int playerIDs[numPlayers]; // Stores playerIDs (might remove later)
 
+    // Create a thread for each player, and let their logic play out
     for (int t = 0; t < numPlayers; t++){
         playerIDs[t] = t;
 
-        if (pthread_create(&threads[t], NULL, playerThread, (void *)&playerIDs[t]) != 0) {
+        struct thread_args newThread = { .playerID = t, .numRounds = numRounds };
+
+        if (pthread_create(&threads[t], NULL, playerThread, (void *)&newThread) != 0) {
             printf("Error creating thread for player %d\n", t);
             return 1;
         }
     }
 
+    // Wait for all of the player threads to finish
     for (int i = 0; i < numPlayers; i++){
         pthread_join(threads[i], NULL);
     }
 
-    // The parent process should wait until all child processes/players are done before exiting
-    // for (int i = 0; i < numPlayers; i++) {
-    //     waitpid(pids[i], NULL, 0);
-    // }
 
-    // In each round:
-    // One player is dealer:
-        // Shuffle cards
-        // Choose random card (Greasy card)
-        // Deal single card to each player
-        // Open first bag of potato chips
-        // Wait for round to end
     
     // Players eat random number of chips bt [1,5] after playing, before next turn
     // -> Only one player can grab chips at a time
     // When a player finds there's no chips left, they open a new bag.
 
-    // There will be one thread per player
-
-    // Each round (non-dealers)
+    // Each round (non-dealers):
 
 
 
